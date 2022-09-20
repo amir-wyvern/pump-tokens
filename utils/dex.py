48b515f843fdaa979b22b1bc9feed7cec7a57ecc
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-09-20 18:54:38 +0430

    update file

--- utils/dex.py
+++ utils/dex.py
@@ -1,47 +1,49 @@
 from web3.middleware import geth_poa_middleware
-from web3 import Web3
 from time import time 
 import logging
-
+from web3 import Web3
 
 class DexTrade :
 
     def __init__(self,
-        endpoint,
+        provider,
         account_keys,
-        tokenA_address,
-        tokenB_address, 
+        token0_address,
+        token1_address, 
         factory_contract,
         router_contract,
-        pair_abi,
+        erc20_abi,
         slippage= 0.01,
         gas_limit= 10165700,
-        gsa_price= 120*10**9
         ):
 
-        self.tokenA_address = Web3.toChecksumAddress(tokenA_address)
-        self.tokenB_address = Web3.toChecksumAddress(tokenB_address)
+
         self.account_keys = account_keys
         self.slippage = slippage
-        self.endpoint = endpoint
         self.gas_limit = gas_limit
-        self.gas_price = gsa_price
-
         self._hexadem = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5' # used for create pair
-        
-        self.w3 = Web3(Web3.HTTPProvider(endpoint))
+        self.WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+
+        self.w3 = Web3(Web3.HTTPProvider(provider))
         self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    
+        self.token0 = self.w3.eth.contract(
+            address= Web3.toChecksumAddress(token0_address),
+            abi= erc20_abi
+        )
+        self.token1 = self.w3.eth.contract(
+            address= Web3.toChecksumAddress(token1_address),
+            abi= erc20_abi
+        )
         
+        setattr(self.token0, "decimal", self.token0.functions.decimals().call())
+        setattr(self.token1, "decimal", self.token1.functions.decimals().call())
+        setattr(self.token0, "symbol", self.token0.functions.symbol().call())
+        setattr(self.token1, "symbol", self.token1.functions.symbol().call())
+
         self.pointer_to_factory_contract = self.w3.eth.contract(
             address= Web3.toChecksumAddress( factory_contract['address'] ),
             abi= factory_contract['abi']
-        )
-
-        self.pairAddress = self.pointer_to_factory_contract.functions.getPair(self.tokenA_address, self.tokenB_address).call()
-        
-        self.pointer_to_lp_contract = self.w3.eth.contract(
-            address= self.pairAddress,
-            abi= pair_abi
         )
 
         self.pointer_to_router_contract = self.w3.eth.contract(
@@ -49,184 +51,303 @@ class DexTrade :
             abi= router_contract['abi']
         )
 
-    def get_tokenA_price(self):
-        
-        # tokenA / tokenB
-
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-        tokenA_price = tokenB_balance / tokenA_balance 
-
-        return tokenA_price
-
-    def get_tokenB_price(self):
-
-        # tokenB / tokenA
-
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-        tokenB_price = tokenA_balance / tokenB_balance 
-
-        return tokenB_price
-
     def get_deadLine(self):
 
-        return int(time()) + 10 * 60
+        return int(time()) + (10 * 60)
 
-    def _allowance(self, object_token):
+    def get_route(self, tokenA, tokenB):
+
+        route = [tokenA, tokenB]
+        if self.WBNB not in route:
+            route = [tokenA, self.WBNB, tokenB]
+
+        return route
+
+    def _allowance(self, token):
         
-        allowance_amount = object_token.functions.allowance(
+        allowance_amount = token.functions.allowance(
             self.account_keys.public_key,
             self.pointer_to_router_contract.address
             ).call()
 
         return allowance_amount
 
-    def _approve(self,object_token ,amount):
+    def _approve(self,token ,amount):
         
-        logging.info('Approve token[{0}] ]'.format(object_token.address))
+        logging.info('Approving...  token[{0}-{1}] ]'.format(token.symbol, token.address))
 
         gas_price = self.w3.eth.gas_price
         nonce = self.w3.eth.get_transaction_count(Web3.toChecksumAddress(self.account_keys.public_key))
         
-        data_field = object_token.functions.approve(self.pointer_to_router_contract.address, amount).buildTransaction({
-        'from': self.account_keys.public_key,
-        'gasPrice': gas_price,
-        'nonce': nonce,
+        data_field = token.functions.approve(self.pointer_to_router_contract.address, amount).buildTransaction({
+            'from': self.account_keys.public_key,
+            'gasPrice': gas_price,
+            'nonce': nonce,
         })
 
         signed_txn = self.w3.eth.account.sign_transaction(data_field, private_key= self.account_keys.private_key)
         tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
         
-        logging.info('Tx approve hash [{0}]'.format(tx_hash.hex()))
         
         self.w3.eth.wait_for_transaction_receipt(tx_hash.hex())
+        logging.info('Tx approve hash [{0}]'.format(tx_hash.hex()))
 
         return tx_hash.hex()
 
-    def check_approve(self, token_address, amount):
+    def check_approve(self, token, amount):
 
-        object_token = self.w3.eth.contract(
-            address= token_address,
-            abi= self.pointer_to_lp_contract.abi
+        if self._allowance(token) < amount:
+            max_amount = 115792089237316195423570985008687907853269984665640564039457584007913129639935
+            self._approve(token, max_amount)
+
+    def get_swap_type(self, token0, token1, swapType):
+
+        if self.WBNB == token0:
+            return swapType.format('ETH', 'Tokens')
+
+        elif self.WBNB == token1:
+            return swapType.format('Tokens', 'ETH')
+
+        else :
+            return swapType.format('Tokens', 'Tokens')
+
+    def get_token0_price_for_buy(self):
+
+        return self.pointer_to_router_contract.functions.getAmountsIn(
+            10**self.token1.decimal,
+            self.get_route(self.token1.address ,self.token0.address)
+        ).call()[0] / 10**self.token0.decimal
+
+    def get_token0_price_for_sell(self):
+
+        return self.pointer_to_router_contract.functions.getAmountsOut(
+            10**self.token1.decimal,
+            self.get_route(self.token0.address ,self.token1.address)
+        ).call()[-1] / 10**self.token0.decimal
+
+    def get_token1_price_for_buy(self):
+
+        return self.pointer_to_router_contract.functions.getAmountsIn(
+            10**self.token0.decimal,
+            self.get_route(self.token0.address ,self.token1.address)
+        ).call()[0] / 10**self.token1.decimal
+
+    def get_token1_price_for_sell(self):
+
+        return self.pointer_to_router_contract.functions.getAmountsOut(
+            10**self.token1.decimal,
+            self.get_route(self.token1.address ,self.token0.address)
+        ).call()[-1] / 10**self.token0.decimal
+
+    def buy_token0(self, amount):
+
+        token1_amount = self.pointer_to_router_contract.functions.getAmountsIn(
+            int(amount*10**self.token0.decimal),
+            self.get_route(self.token1.address ,self.token0.address) ).call()[0]
+
+        token1_amount = int((1 + self.slippage) * token1_amount) 
+
+        swapType = 'swap{0}ForExact{1}'
+        swapType = self.get_swap_type(self.token1.address, self.token0.address, swapType)
+        
+        self.check_approve(self.token1, token1_amount)
+
+        logging.info('Sawping...  token1[{0}|{1}] -> token0[{2}|{3}]'.format(
+            self.token1.symbol,
+            round(float(token1_amount)/10**self.token1.decimal, 5),
+            self.token0.symbol,
+            round(float(amount),5),
+            )
         )
 
-        if self._allowance(object_token) < amount:
-            max_amount = 115792089237316195423570985008687907853269984665640564039457584007913129639935
-            self._approve(object_token, max_amount)
+        route= self.get_route(self.token1.address ,self.token0.address)
+        dict_args = {}
+        if swapType == 'swapETHForExactTokens':
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token0.decimal),
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
+            
+            dict_args['value'] = token1_amount
 
-
-    def sell_tokenA(self, amount):
-
-
-        tokenA_amount = Web3.toWei(amount, 'ether')
-
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-
-        tokenB_amount = Web3.toWei(tokenB_balance / tokenA_balance * amount, 'ether')
-
-        tokenB_amount = int((1 - self.slippage) * tokenB_amount) 
-
-        logging.info('Sawp tokenA[{0:3.3f}] -> tokenB[{1:3.3f}]'.format(amount, tokenB_amount/10**18))
-
-        self.check_approve(self.tokenA_address, tokenA_amount)
-
-        return self._make_trade(
-            fn_identifier='swapExactTokensForTokens',
-            input_token_amount= tokenA_amount,
-            output_token_amount= tokenB_amount,
-            input_token_address= self.tokenA_address,
-            output_token_address= self.tokenB_address
-            )
-
-    def sell_tokenB(self, amount):
-
-        tokenB_amount = Web3.toWei(amount, 'ether')
-
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-        tokenA_amount = Web3.toWei(tokenA_balance / tokenB_balance * amount, 'ether')
-        tokenA_amount = int((1 - self.slippage) * tokenA_amount) 
-
-        logging.info('Sawp tokenB[{0:3.3f}] -> tokenA[{1:3.3f}]'.format(amount, tokenA_amount/10**18) )
-
-        self.check_approve(self.tokenB_address, tokenB_amount)
+        else:
+            
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token0.decimal), 
+                    token1_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
 
         return self._make_trade(
-            fn_identifier='swapExactTokensForTokens',
-            input_token_amount= tokenB_amount,
-            output_token_amount= tokenA_amount,
-            input_token_address= self.tokenB_address,
-            output_token_address= self.tokenA_address
+            fn_identifier= obj,
+            dict_args= dict_args
             )
 
-    def buy_tokenA(self, amount):
+    def buy_token1(self, amount):
 
-        tokenA_amount = Web3.toWei(amount, 'ether')
+        token0_amount = self.pointer_to_router_contract.functions.getAmountsIn(
+            int(amount*10**self.token1.decimal),
+            self.get_route(self.token0.address ,self.token1.address) ).call()[0]
 
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-        tokenB_amount = Web3.toWei(tokenB_balance / tokenA_balance * amount, 'ether')
+        token0_amount = int((1 + self.slippage) * token0_amount) 
 
-        tokenB_amount = int((1 + self.slippage) * tokenB_amount) 
+        swapType = 'swap{0}ForExact{1}'
+        swapType = self.get_swap_type(self.token0.address, self.token1.address, swapType)
         
-        logging.info('Sawp tokenB[{0:3.3f}] -> tokenA[{1:3.3f}]'.format(tokenB_amount/10**18, amount) )
+        self.check_approve(self.token0, token0_amount)
 
-        self.check_approve(self.tokenB_address, tokenB_amount)
+        logging.info('Sawping...  token0[{0}|{1}] -> token1[{2}|{3}]'.format(
+            self.token0.symbol,
+            round(float(token0_amount)/10**self.token0.decimal, 5),
+            self.token1.symbol,
+            round(float(amount),5),
+            )
+        )
+
+        route= self.get_route(self.token0.address ,self.token1.address)
+        dict_args = {}
+        if swapType == 'swapETHForExactTokens':
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token1.decimal),
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
+            
+            dict_args['value'] = token0_amount
+
+        else:
+            
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token1.decimal), 
+                    token0_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
 
         return self._make_trade(
-            fn_identifier='swapTokensForExactTokens',
-            input_token_amount= tokenA_amount,
-            output_token_amount= tokenB_amount,
-            input_token_address= self.tokenB_address,
-            output_token_address= self.tokenA_address
+            fn_identifier= obj,
+            dict_args= dict_args
             )
 
-    def buy_tokenB(self, amount):
+    def sell_token0(self, amount):
 
+        token1_amount = self.pointer_to_router_contract.functions.getAmountsOut(
+            int(amount*10**self.token0.decimal),
+            self.get_route(self.token0.address ,self.token1.address) ).call()[-1]
 
-        tokenB_amount = Web3.toWei(amount, 'ether')
+        token1_amount = int((1 - self.slippage) * token1_amount) 
 
-        [tokenA_balance ,tokenB_balance ,_] = self.pointer_to_lp_contract.functions.getReserves().call()
-        tokenA_amount = Web3.toWei(tokenA_balance / tokenB_balance * amount, 'ether')
+        swapType = 'swapExact{0}For{1}'
+        swapType = self.get_swap_type(self.token0.address, self.token1.address, swapType)
+        
+        self.check_approve(self.token0, int(amount*10**self.token0.decimal))
 
-        tokenA_amount = int((1 + self.slippage) * tokenA_amount) 
+        logging.info('Sawping...  token0[{0}|{1}] -> token1[{2}|{3}]'.format(
+            self.token0.symbol,
+            round(float(amount),5),
+            self.token1.symbol,
+            round(float(token1_amount)/10**self.token1.decimal, 5),
+            )
+        )
 
-        logging.info('Sawp tokenA[{0:3.3f}] -> tokenB[{1:3.3f}]'.format(tokenA_amount/10**18 ,amount) )
+        route = self.get_route(self.token0.address ,self.token1.address)
+        dict_args = {}
 
-        self.check_approve(self.tokenA_address, tokenA_amount)
+        if swapType == 'swapExactETHForTokens':
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    token1_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
+            
+            dict_args['value'] = int(amount * 10 ** self.token0.decimal)
+
+        else:
+            
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token0.decimal), 
+                    token1_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
 
         return self._make_trade(
-            fn_identifier='swapTokensForExactTokens',
-            input_token_amount= tokenB_amount,
-            output_token_amount= tokenA_amount,
-            input_token_address= self.tokenA_address,
-            output_token_address= self.tokenB_address
+            fn_identifier= obj,
+            dict_args= dict_args
+            )
+
+    def sell_token1(self, amount):
+
+        token0_amount = self.pointer_to_router_contract.functions.getAmountsOut(
+            int(amount*10**self.token1.decimal),
+            self.get_route(self.token1.address, self.token0.address) ).call()[-1]
+
+        token0_amount = int((1 - self.slippage) * token0_amount) 
+
+        swapType = 'swapExact{0}For{1}'
+        swapType = self.get_swap_type(self.token1.address, self.token0.address, swapType)
+        
+        self.check_approve(self.token1, int(amount*10**self.token1.decimal))
+
+        logging.info('Sawping...  token1[{0}|{1}] -> token0[{2}|{3}]'.format(
+            self.token1.symbol,
+            round(float(amount),5),
+            self.token0.symbol,
+            round(float(token0_amount)/10**self.token0.decimal, 5),
+            )
+        )
+
+        route = self.get_route(self.token1.address ,self.token0.address)
+
+        dict_args = {}
+        if swapType == 'swapExactETHForTokens':
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    token0_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
+            
+            dict_args['value'] = int(amount * 10 ** self.token1.decimal)
+
+        else:
+            
+            obj = getattr(self.pointer_to_router_contract.functions, swapType)(
+                    int(amount * 10 ** self.token1.decimal), 
+                    token0_amount,
+                    route,
+                    Web3.toChecksumAddress(self.account_keys.public_key),
+                    self.get_deadLine()
+                )
+
+        return self._make_trade(
+            fn_identifier= obj,
+            dict_args= dict_args
             )
 
     def _make_trade(
         self,
         fn_identifier,
-        input_token_amount,
-        output_token_amount,
-        input_token_address, 
-        output_token_address
+        dict_args
         ):
             
-        gas_price = self.w3.eth.gas_price
-        nonce = self.w3.eth.get_transaction_count(Web3.toChecksumAddress(self.account_keys.public_key))
-        
-        data_field = getattr(self.pointer_to_router_contract.functions, fn_identifier)(
-                input_token_amount,
-                output_token_amount, 
-                [input_token_address,output_token_address],
-                Web3.toChecksumAddress(self.account_keys.public_key),
-                int(self.get_deadLine())
-            ).buildTransaction({
-                'from': Web3.toChecksumAddress(self.account_keys.public_key),
-                'gasPrice': gas_price,
-                'nonce': nonce,
-            })
+        dict_args["from"] = Web3.toChecksumAddress(self.account_keys.public_key)
+        dict_args["gasPrice"] = self.w3.eth.gas_price
+        dict_args["nonce"] = self.w3.eth.get_transaction_count(Web3.toChecksumAddress(self.account_keys.public_key))
+
+        data_field = fn_identifier.buildTransaction(dict_args)
 
         signed_txn = self.w3.eth.account.sign_transaction(data_field, private_key=self.account_keys.private_key)
+
         tx_hash = self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        
         logging.info('Tx hash [{0}]'.format(tx_hash.hex()))
 
         return tx_hash.hex()
