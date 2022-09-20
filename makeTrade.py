e9b8f528653cf6c03fecebdcebe207a232d1815d
amir-wyvern <amirhosein_wyvern@yahoo.com>
2022-09-20 18:56:09 +0430

    update file

--- makeTrade.py
+++ makeTrade.py
@@ -1,7 +1,12 @@
+
+from utils.blockchainAccountBalance import BlockchainBalance
 from utils.contracts import load_contracs
 from utils.account import Account
-from utils.dex import DexTrade
+
 from dotenv import load_dotenv
+
+from dex import DexTrade
+
 import logging
 import os
 
@@ -9,42 +14,43 @@ logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)-24s] [%(levelname)-8s] [%(lineno)-3d -%(name)-7s] | %(message)s',
     handlers=[
-    logging.FileHandler("dexTrade.log"),
+    logging.FileHandler("trade.log"),
     logging.StreamHandler()
     ]
 )
 
-
+import uniswap
 if __name__ == '__main__':
 
-    tokenB_address = '' # busd
-    tokenA_address = '' # wbnb
-    amount = 0
+    token0_address = '' 
+    token1_address = ''
 
     load_dotenv()
-
     password = os.getenv('PASSWORD').encode()
+    
     account_keys = Account(password)
 
     list_contracts = load_contracs()
 
     dex = DexTrade(
-        endpoint= 'https://bsc-dataseed.binance.org',
+        provider= 'https://bsc-dataseed.binance.org',
         account_keys= account_keys,
-        tokenA_address= tokenA_address,
-        tokenB_address= tokenB_address,
+        token0_address= token0_address,
+        token1_address= token1_address,
         factory_contract= list_contracts['pancake-factory'],
         router_contract= list_contracts['router'],
-        pair_abi= list_contracts['pancake-pair']['abi'],
-        slippage=49
-        )
+        erc20_abi= list_contracts['erc20']['abi'],
+        slippage= 0.01)
 
-    # dex.check_approve(token_address, amount)
 
-    # dex.get_tokenA_price()
-    # dex.get_tokenB_price()
+    # print(dex.get_token0_price_for_buy() )
+    # print(dex.get_token0_price_for_sell() )
+    # print(dex.get_token1_price_for_buy() )
+    # print(dex.get_token1_price_for_sell() )
 
-    # dex.buy_tokenA(amount)
-    # dex.buy_tokenB(amount)
-    # dex.sell_tokenA(amount)
-    # dex.sell_tokenB(amount)    
+    # print(dex.buy_token0(amount=1))
+    # print(dex.buy_token1(amount=1))
+    # print(dex.sell_token0(amount=1))
+    # print(dex.sell_token1(amount=1))
+
+
